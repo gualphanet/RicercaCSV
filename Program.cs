29b569dce4005356c0093213ddbc774307b04bc7
gualphanet <127530247+gualphanet@users.noreply.github.com>
2023-03-10 16:28:46 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace RicercaCSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            string[] fields;
+
+            fields = new String[4];
+
+            try
+            {
+                //apertura file
+                StreamReader sr = new StreamReader(args[0]);
+                
+                //lettura prima riga
+                line = sr.ReadLine();
+                
+                while (line != null)
+                {
+                    //separazione campi riga
+                    fields = line.Split(',',';');
+
+                    //controllo match con chiave di ricerca in colonna specificata
+                    if (fields[Int32.Parse(args[1])].Equals(args[2]))
+                    {
+                        //stampa riga in caso di match
+                        Console.WriteLine(line);
+                    }
+                    
+                    //lettura prossima riga
+                    line = sr.ReadLine();
+                }
+                
+                //chiusura file
+                sr.Close();
+                
+            }
+            //controllo errori
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            
+        }
+    }
+}
